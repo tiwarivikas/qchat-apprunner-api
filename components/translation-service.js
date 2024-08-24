05925857d6ab7ed0d1ed203fc8a2fafd634a8503
Vikas Tiwari <tiwvika@amazon.com>
2024-08-24 23:46:35 +0530

    fix: compile errors

--- components/translation-service.js
+++ components/translation-service.js
@@ -1,22 +1,27 @@
-const { TranslateClient, TranslateTextCommand } = require('@aws-sdk/client-translate');
+const {
+  TranslateClient,
+  TranslateTextCommand,
+} = require("@aws-sdk/client-translate");
 
 const translateClient = new TranslateClient({});
 
 // Function to translate text
 async function translateText(text, sourceLanguageCode, targetLanguageCode) {
-    const command = new TranslateTextCommand({
-      Text: text,
-      SourceLanguageCode: sourceLanguageCode,
-      TargetLanguageCode: targetLanguageCode,
-    });
-  
-    try {
-      const response = await translateClient.send(command);
-      return response.TranslatedText;
-    } catch (error) {
-      console.error('Error translating text:', error);
-      throw error;
-    }
-  }
+  if (sourceLanguageCode == targetLanguageCode) return text;
 
-  module.exports = {translateText}
+  const command = new TranslateTextCommand({
+    Text: text,
+    SourceLanguageCode: sourceLanguageCode,
+    TargetLanguageCode: targetLanguageCode,
+  });
+
+  try {
+    const response = await translateClient.send(command);
+    return response.TranslatedText;
+  } catch (error) {
+    console.error("Error translating text:", error);
+    throw error;
+  }
+}
+
+module.exports = { translateText };
